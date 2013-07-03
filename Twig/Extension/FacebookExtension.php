bf2e38589136b4474372e2993bac1af4e2840f26
Yasmany Cubela <ycubela@smartappsla.com>
2013-07-03 10:04:48 -0430

    fixing twig ext

--- Twig/Extension/FacebookExtension.php
+++ Twig/Extension/FacebookExtension.php
@@ -35,84 +35,56 @@ class FacebookExtension extends \Twig_Extension
   
   public function getFunctions( )
   {
-    return array( 
-        'facebook_initialize' => new \Twig_Function_Method( $this, 'renderInitialize',
-            array( 'is_safe' => array( 'html' ) )),
-        'facebook_login_button' => new \Twig_Function_Method( $this, 'renderLoginButton',
-            array( 'is_safe' => array( 'html' ) )),
-        'facebook_scope' => new \Twig_Function_Method( $this, 'renderScope', array( 'is_safe' => array( 'html' ) )),
-        'facebook_login_function' => new \Twig_Function_Method( $this, 'renderLoginFunction',
-            array( 'is_safe' => array( 'html' ) )),
-        'facebook_login_url' => new \Twig_Function_Method( $this, 'renderLoginUrl',
-            array( 'is_safe' => array( 'html' ) )),
-        'facebook_logout_url' => new \Twig_Function_Method( $this, 'renderLogoutUrl',
-            array( 'is_safe' => array( 'html' ) )), );
+    $extra = array( 'is_safe' => array( 'html' ) );
+    
+    $functions = array( );
+    $functions[ 'facebook_initialize' ] = new \Twig_Function_Method( $this, 'renderInitialize', $extra);
+    $functions[ 'facebook_login_button' ] = new \Twig_Function_Method( $this, 'renderLoginButton', $extra);
+    $functions[ 'facebook_scope' ] = new \Twig_Function_Method( $this, 'renderScope', $extra);
+    $functions[ 'facebook_login_function' ] = new \Twig_Function_Method( $this, 'renderLoginFunction', $extra);
+    $functions[ 'facebook_login_url' ] = new \Twig_Function_Method( $this, 'renderLoginUrl', $extra);
+    $functions[ 'facebook_logout_url' ] = new \Twig_Function_Method( $this, 'renderLogoutUrl', $extra);
+    
+    return $functions;
   }
   
-  /**
-   * Returns the name of the extension.
-   *
-   * @return string The extension name
-   */
+  private function helper( )
+  {
+    return $helper = $this->container->get( 'bit_facebook.helper' );
+  }
+  
+  public function renderInitialize( $parameters = array( ), $name = null )
+  {
+    return $this->helper( )->initialize( $parameters, $name ? : 'FOSFacebookBundle::initialize.html.twig' );
+  }
+  
+  public function renderLoginButton( $parameters = array( ), $name = null )
+  {
+    return $this->helper( )->loginButton( $parameters, $name ? : 'FOSFacebookBundle::loginButton.html.twig' );
+  }
+  
+  public function renderScope( )
+  {
+    return $this->helper( )->scope( );
+  }
+  
+  public function renderLoginFunction( $loginCheck )
+  {
+    return $this->helper( )->loginFunction( $loginCheck );
+  }
+  
+  public function renderLoginUrl( $redirectUtl, $parameters = array( ) )
+  {
+    return $this->helper( )->loginUrl( $redirectUtl, $parameters );
+  }
+  
+  public function renderLogoutUrl( $parameters = array( ), $name = null )
+  {
+    return $this->helper( )->logoutUrl( $parameters );
+  }
   
   public function getName( )
   {
     return 'facebook';
-  }
-  
-  /**
-   * @see FacebookHelper::initialize()
-   */
-  
-  public function renderInitialize( $parameters = array( ), $name = null )
-  {
-    return $this->container->get( 'fos_facebook.helper' )
-        ->initialize( $parameters, $name ? : 'FOSFacebookBundle::initialize.html.twig' );
-  }
-  
-  /**
-   * @see FacebookHelper::loginButton()
-   */
-  
-  public function renderLoginButton( $parameters = array( ), $name = null )
-  {
-    return $this->container->get( 'fos_facebook.helper' )
-        ->loginButton( $parameters, $name ? : 'FOSFacebookBundle::loginButton.html.twig' );
-  }
-  
-  /**
-   * @see FacebookHelper::scope()
-   */
-  
-  public function renderScope( )
-  {
-    return $this->container->get( 'fos_facebook.helper' )->scope( );
-  }
-  
-  /**
-   * @see FacebookHelper::loginFunction()
-   */
-  
-  public function renderLoginFunction( $loginCheck )
-  {
-    return $this->container->get( 'fos_facebook.helper' )->loginFunction( $loginCheck );
-  }
-  
-  /**
-   * @see FacebookHelper::authUrl()
-   */
-  
-  public function renderLoginUrl( $redirectUtl, $parameters = array( ) )
-  {
-    return $this->container->get( 'fos_facebook.helper' )->loginUrl( $redirectUtl, $parameters );
-  }
-  
-  /**
-   * @see FacebookHelper::logoutUrl()
-   */
-  
-  public function renderLogoutUrl( $parameters = array( ), $name = null )
-  {
-    return $this->container->get( 'fos_facebook.helper' )->logoutUrl( $parameters );
   }
 }
