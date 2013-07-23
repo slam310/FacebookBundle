c22030b3ed269a4133e90280d76f00b6e7f6e806
Yasmany Cubela <ycubela@smartappsla.com>
2013-07-23 10:56:16 -0430

    adding function to check facebooklogin status

--- Twig/Extension/FacebookExtension.php
+++ Twig/Extension/FacebookExtension.php
@@ -42,7 +42,7 @@ class FacebookExtension extends \Twig_Extension
     $functions[ 'facebook_login_button' ] = new \Twig_Function_Method( $this, 'renderLoginButton', $extra);
     $functions[ 'facebook_scope' ] = new \Twig_Function_Method( $this, 'renderScope', $extra);
     $functions[ 'facebook_login_function' ] = new \Twig_Function_Method( $this, 'renderLoginFunction', $extra);
-    $functions[ 'facebook_check_login_status' ] = new \Twig_Function_Method( $this, 'renderCheckFBLoginStatusFunciton',
+    $functions[ 'facebook_check_login_status' ] = new \Twig_Function_Method( $this, 'renderCheckFBLoginStatusFunction',
         $extra);
     $functions[ 'facebook_login_url' ] = new \Twig_Function_Method( $this, 'renderLoginUrl', $extra);
     $functions[ 'facebook_logout_url' ] = new \Twig_Function_Method( $this, 'renderLogoutUrl', $extra);
