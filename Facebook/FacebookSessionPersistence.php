a3d989cb87f12b2028c7edb8c94f16b7c24a895b
Yasmany Cubela Medina <yasmanycm@gmail.com>
2013-10-15 09:47:07 -0430

    fixing session bug

--- Facebook/FacebookSessionPersistence.php
+++ Facebook/FacebookSessionPersistence.php
@@ -32,7 +32,6 @@ class FacebookSessionPersistence extends \BaseFacebook
   {
     $this->session = $session;
     $this->prefix = $prefix;
-    $this->session->start( );
     
     parent::__construct( $config );
   }
